f107aba3f4b9e077794635f362eb5bd5d77610b7
Moth <usualmoth@gmail.com>
2025-11-27 01:44:25 +0100

    yourscore

--- js/stats.js
+++ js/stats.js
@@ -1,8 +1,12 @@
 const scores = JSON.parse(localStorage.getItem("gameScore")) || [];
 
 const bestScoreElement = document.getElementById("bestScore");
+const scoreElement = document.getElementById("score");
 
 if (scores.length > 0) {
   const bestScore = Math.max(...scores.map(game => game.score));
   bestScoreElement.textContent = bestScore;
+
+  const lastGame = scores[scores.length - 1];
+  scoreElement.textContent = lastGame.score;
 } 
